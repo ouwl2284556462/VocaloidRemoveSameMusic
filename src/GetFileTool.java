7e5d0e9dc53c54eead3f356518ea0a5eb8525860
ouwl2284556462 <361516711@qq.com>
2020-06-22 22:59:02 +0800

    修改目录问题

--- src/GetFileTool.java
+++ src/GetFileTool.java
@@ -7,20 +7,55 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class GetFileTool {
+	
+	
+	
 
 	public static void main(String[] args) throws Exception {
-		List<File> collector = new ArrayList<File>();
 		
-		collectAllFile("E:\\voaloid_temp\\NEW", collector);
-		//printAllFileName(collector);
-		
-		//移动文件
-		//String moveToDir = "E:\\voaloid_temp\\NEW\\";
-		//moveToDir(collector, moveToDir);
+		//将srcPath目录下的所有文件（包括子目录下的文件）移动到tarPath目录下
+		moveMusic("G:\\voaloid_temp\\NEW", "G:\\voaloid_temp\\NEW");
 		
 		//删除重复音乐
-		removeSameMusic(collector);
+		//removeSameMusicFile("G:\\voaloid_temp\\NEW");
 		
+		//moveZip("G:\\voaloid_temp\\NEW", "G:\\voaloid_temp\\NEW");
+	}
+
+
+	/**
+	 * 将源目录下的所有zip文件移动到目标目录
+	 * @param srcPath
+	 * @param tarPath
+	 * @throws Exception 
+	 */
+	private static void moveZip(String srcPath, String tarPath) throws Exception {
+		List<File> collector = new ArrayList<File>();
+		collectAllZipFile(srcPath, collector);
+		//移动文件
+		moveToDir(collector, tarPath);
+		System.out.println("移动完成");		
+	}
+
+
+	private static void moveMusic(String srcPath, String tarPath) throws Exception {
+		List<File> collector = new ArrayList<File>();
+		collectAllFile(srcPath, collector);
+		//移动文件
+		moveToDir(collector, tarPath);
+		System.out.println("移动完成");
+	}
+
+
+	/**
+	 * 删除重复音乐
+	 * @param path
+	 * @return
+	 */
+	private static void removeSameMusicFile(String path) {
+		List<File> collector = new ArrayList<File>();
+		collectAllFile(path, collector);
+		removeSameMusic(collector);
 	}
 
 
@@ -69,6 +104,8 @@ public class GetFileTool {
 				}
 			}
 		}
+		
+		System.out.println("执行成功");
 	}
 
 
@@ -76,7 +113,7 @@ public class GetFileTool {
 		
 		
 		for(File file : collector){
-			File destDir = new File(moveToDir  + file.getName());
+			File destDir = new File(moveToDir + "\\"  + file.getName());
 			
 			if(file.getAbsolutePath().equals(destDir.getAbsolutePath())){
 				System.out.println("不需要移动" + destDir.getAbsolutePath());
@@ -101,6 +138,25 @@ public class GetFileTool {
 		System.out.println(collector.size());
 		for(File file : collector){
 			System.out.println(file.getName());
+		}
+	}
+	
+	
+	private static void collectAllZipFile(String path, List<File> collector) {
+		File[] fileList = new File(path).listFiles();
+		for(File file : fileList){
+			if (file.isDirectory()) {
+				collectAllFile(file.getAbsolutePath(), collector);
+				continue;
+			}
+			
+			String fileName = file.getName().toLowerCase();
+			if(!fileName.endsWith(".7z") && !fileName.endsWith(".zip")) {
+				continue;
+			}
+			
+			
+			collector.add(file);
 		}
 	}
 	
